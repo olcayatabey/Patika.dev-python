9c0e0cdd338c4ab7e98a1f3e2dbbe1105d243c92
Olcay ATABEY <64277413+olcayatabey@users.noreply.github.com>
2021-07-25 17:35:48 +0300

    Update flatten.py

--- flatten.py
+++ flatten.py
@@ -1,17 +1,19 @@
 def flatten(list1):
-    flattenList = []
+    #boş bir liste oluşturuyoruz.
+    newList = []
     #Listemizi döngüye alıp, elemanları bakıyoruz.
     for e in list1:
         #Eğer listenin elemanı, başka bir liste değilse flattenList'imize listemize ekliyoruz.
         if type(e) != type([]):
-            flattenList.append(e)
+            newList.append(e)
         #Eğer listenin elemanı, başka bir liste ise, bu sefer onu flatten fonksiyonuna sokuyoruz ve 
         # liste olma özelliğini kaybedene kadar bu işlemi yapıyoruz. En sonunda flattenList'imize ekliyoruz.
         else:
-            flattenList.extend(flatten(e))
-    return flattenList
+            newList.extend(flatten(e))
+    return newList
     
 
 list1 = [[1, 'a', ['cat'], 2], [[[3]], 'dog'], 4, 5]
 list1=flatten(list1)
 print(list1)
+#output [1, 'a', 'cat', 2, 3, 'dog', 4, 5]
