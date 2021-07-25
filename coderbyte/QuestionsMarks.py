8c9b60c974fd7f7f753e8c1027034095c869d3d5
Olcay ATABEY <64277413+olcayatabey@users.noreply.github.com>
2021-07-25 21:39:40 +0300

    Create QuestionsMarks.py

---
+++ coderbyte/QuestionsMarks.py
@@ -0,0 +1,20 @@
+def QuestionsMarks(str):
+
+  a = 0
+  b = 'false'
+  c = 0
+  for i in str:
+    if i.isdigit():
+      if int(i) + a == 10:
+        b='true'
+        if c != 3:
+          return 'false'
+        b = 'true'
+        c = 0
+        a = int(i)
+    elif i == '?':
+      c += 1
+  return b
+
+# keep this function call here 
+print(QuestionsMarks(input()))
