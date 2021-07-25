f9ca1536fa3e0f975e4842e477904f38405e6a48
Olcay ATABEY <64277413+olcayatabey@users.noreply.github.com>
2021-07-25 21:09:32 +0300

    Create BracketMatcher.py

---
+++ coderbyte/BracketMatcher.py
@@ -0,0 +1,14 @@
+def BracketMatcher(strParam):
+  count=0
+  for c in strParam:
+    if c=='(':
+      count+=1
+    elif c==')':
+      count-=1
+    if count!=0:
+      return 0
+    elif count==0:
+      return 1
+
+# keep this function call here 
+print(BracketMatcher(input()))
